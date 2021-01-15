a877f5a926f1ebf573ad0c9b03a46c4bbcf359c6
Brando <brandto.tomalav@outlook.com>
2021-01-14 20:28:24 -0500

    Config url_BD

--- server/config/config.js
+++ server/config/config.js
@@ -8,7 +8,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-if (process.env.NODE_ENV !== 'dev') {
+if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
     urlDB = 'mongodb+srv://Brando_T:y3vIJtkfDPE9upoD@cluster0.mbmhh.mongodb.net/cafe'
